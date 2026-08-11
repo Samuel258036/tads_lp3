e2890d0046f193f0869eb8b5a8465c99021b7881
Samuel P. Silva <prado.silva@aluno.ifsp.edu.br>
2026-08-10 22:26:01 -0300

    aula-03: dominio, contratos e fluxo sincrono

---
+++ src/main/java/br/edu/ifsp/orderflow/domain/Cliente.java
@@ -0,0 +1,31 @@
+package br.edu.ifsp.orderflow.domain;
+
+public class Cliente {
+
+    private final String nome;
+    private final String email;
+    private final String id;
+
+    public Cliente(String nome, String email, String id) {
+        this.nome = nome;
+        this.email = email;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return this.nome + " <" + this.email + ">";
+    }
+}
